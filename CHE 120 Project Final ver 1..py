24fa932b87244e74e2ce1caa4bc513d91246c498
synguyentran06 <sn2tran@uwaterloo.ca>
2025-11-25 13:41:09 -0500

    Integrated Sy's 2 faetures, and Haris's feature

--- CHE 120 Project Final ver 1..py
+++ CHE 120 Project Final ver 1..py
@@ -593,37 +593,272 @@ def move_2():
 
 
 def move_3():
+    global aim, pacman, ghosts, tiles, count, move
+
+    if count == 0:
+        """
+        SNT:
+        Since level 4 essentially uses the same map as the first level, so we reuse restart_level_1()
+        """
+        restart_level_1()
+        count += 1
+    """
+    HN:
+    ghosts is created as 
+    ghosts = [
+        [vector(-180, 160), vector(5, 0)],
+        [vector(-180, -160), vector(0, 5)],
+        [vector(100, 160), vector(0, -5)],
+        [vector(100, -160), vector(-5, 0)],
+    ]
+    The second vector, i.e. vector(10,0) is responsible for controlling the speed of the ghosts, where the number 10 corresponds the speed that the ghost travels with.
+    The number can be increased to speed up the ghosts or decreased to speed down the ghosts. For this level, the ghosts are meant to speed up to raise the difficulty,
+    therefore the speed has been increased from 5 to 10.
+    """
+    """
+    SNT:
+    The variable "ghosts" stores 4 new ghosts as sublist, each having two vectors associating to it. The first vector of each sublist is the position vector, the second vector is the velocity vector
+    In the context of this game:
+    (5, 0) go right
+    (0, 5) go up
+    (0, -5) go down
+    (-5, 0) go left
+    """
+    """
+    HN:
+    Tiles is basically the entire map of the game, It is a list containing 1s and 0s where 1 corresponds to a path that the pacman/ghosts can follow
+    and 0 corresponds to a wall, which the pacman/ghost can collide with. The map can be changed by changing the positions of the 1s and 0s in the list.
+    """
+    # fmt: on
+
+    def spawn_ghost():
+        """
+        HN: Spawns a new ghost if possible (7 ghosts is the limit for this difficulty level)
+            the ghosts are able to spawn at 4 different locations (the locations are similar to the inital spawn locations of the ghosts
+            and are able to have one of 4 different directions of movement, which is entirely randomized. Therefore it is not known which 
+            direction the newly spawned ghost will travel in neither is the spawn location known.
+        """
+        # HN: This line is what allows the newly spawned ghosts to spawn at any one of the four given locations. These locations are the same as the other ghosts.
+        if len(ghosts) < 7:
+            possible_spawn_point = [
+                vector(-180, 160),
+                vector(-180, -160),
+                vector(100, 160),
+                vector(100, -160)]
+
+            spawn_point = choice(possible_spawn_point) #HN: Choice allows the game to randomly choose one of the given four spawn points.
+
+            # HN: This list allows us to be able to choose a random direction for the ghosts.
+            speed_options = [
+                vector(10 , 0), #HN: Right
+                vector(0 , 10), #HN: Up
+                vector(0 , -10), #HN: Down
+                vector(-10 , 0), #HN: Left
+                ]
+            
+            chosen_speed = choice(speed_options) #HN: Similarly choice here allows the game to randomly choose one the four given speed options.
+
+            ghosts.append([spawn_point.copy(), chosen_speed]) #HN: Using the append feature, we able to add a new list to the list storing all the information for ghosts.
+
+        ontimer(spawn_ghost, 30000) 
+    
+
+    """Move pacman and all ghosts."""
+    """
+    SNT:
+    The main purpose of this function is to move the pac man and the ghost around.
+    However, some of the code here makes little sense out of the context which it is used.
+    """
+    """
+    HN:
+    This is the main function which is responsible for the entire game. Everytime this function is called, the ghost and pacman move once.
+    """
+    
+    writer.undo() #SNT: Undo the what the previous turtle "writer" wrote.
+    
+    """
+    SNT:
+    In the context in which move() is called, a few line above, the turtle "writer" is commanded to write out the score.
+    As such writer.undo() erased any previously written out score.
+    Furthermore, when move() is called, in itself is a function that keeps on calling move(). As such, move is continously called again and again.
+    Therefore, having writer.undo() is neccesary to erased previously written scores.
+    """
+    
+    writer.write(state['score']) #SNT: Use the turtle "writer" to write out the new score of the game.
+
+    clear()
+    """
+    SNT:
+    This functions clear previous drawing done by the turtles.
+    Without this function, when the ghosts and pacman moves, its creates a trail.
+    We intends to use this functions to create an extra feature in the games.
+    """
+
+    spawn_ghost() #SNT: Call the function spawn ghost
+
+
+    if valid(pacman + aim):
+        pacman.move(aim)
+        
+        """
+        SNT:
+        This function checks if the pacman can moves to a certain place.
+        Understanding that "pacman" and "aims" are both vectors, pactman + aim is essentially the position vector of where the pacman intends to go.
+        valid(pacman + aim) check if the points are valid for the pacman to move into.
+        pacman.move(aim) essentially moves pacman by aim. 
+        """
+
+    index = offset(pacman) #SNT: Locates the location of pacman on the pixel coordinates and returns its index in the list "tiles"
+
+    if tiles[index] == 1:
+        """
+        SNT:
+        This if statement check if the current location of the pacman is in an untraveled path. This allows the games to tally score
+        This is accomplished if the tile's stored value at index in the list "tiles" is 1.
+        If it is, the tile's stored value is changed to 2. And a score is added
+        In the context of the game, 1 is untraveled path, 2 is travelled path.
+        As such, if tiles has already been traveled, its value is 2 so this if statement won't be triggered, and no new points is added.
+        """
+        tiles[index] = 2 #SNT: Update the tile's stored value to 2, so the program knows we traveled over this tiles.
+
+        state['score'] += 1 #SNT: The user score is increase by 1.
+        x = (index % 20) * 20 - 200 #SNT: The next three line redraw the tiles, this time without the "coins" in the middle
+        y = 180 - (index // 20) * 20
+        square(x, y)
+
+
+    advanced_level(1, 1)
+
+
+    """
+    SNT:
+    The next three line moves the turtle up and make it goes to a location 10 upward and 10 horizontally from the pacman vector.
+    To be noted, within the context of this game, a position vector for pacman or ghost lays on gridlinnes, while their actual appearances needed to be in center of the tiles.
+    As such, when pacman is drawn, the drawing coodinates is shifted up 10 and horizontally 10.
+    """
+    up()
+    goto(pacman.x + 10, pacman.y + 10)
+    dot(20, 'yellow')
+
+    for point, course in ghosts:
+        """
+        SNT:
+        This for loop go through each index of the list "ghost". Each time, it accessed and assigned first vector in the sublist in "point" and the second vector in the sublist in "course".
+        "point" is essentially the position vectors of each ghosts.
+        "course" is essentially the velocity vector's of each ghosts.
+        """
+
+        if valid(point + course): #check if the positions where the ghost is aheaded is a valid desitnation
+            point.move(course)
+            """
+            SNT:
+            In this portion of the if statement, valid(point + course) check if the new destination of the ghost is a valid point to move to.
+            If it is, it will move there.
+            """
+        else:
+            """
+            SNT:
+            This portion of the if statements would be activated if the ghost can't move according to its inset velocity vector since said its destination isn't a valid block.
+            Instead, choice() is used to pick one of the random cardinal velocity vector stored in options, and replace the ghost's velocity vector.
+            """
+            """
+            HN:
+            This function has been modified, and the speeds for the ghosts has been increased for difficulty level 2 by changing the value of the vector
+            from (5,0) to (8,0) to make the game more challenging. 
+            """
+            options = [ #SNT: Initialized a list "options", which contains 4 different cardinal velocity vectors
+                vector(10, 0),
+                vector(-10, 0),
+                vector(0, 10),
+                vector(0, -10),
+            ]
+            plan = choice(options) #SNT: A random cardinal velocity vector is picked from "options" and assigned to plan
+            course.x = plan.x #SNT: The course vector is changed into the planned vector.
+            course.y = plan.y
+            """
+            SNT:
+            In the context of this game, this function (move()) is called multiples time, continously in a game. If the ghost can't move in this turn, its velocity is randomly changed.
+            The next time this function is called, the function re-elevaluate if that velocity vector would create a valid motion (aka: don't enter a wall). This keep occuring until a valid direction is randomly picked.
+            """
+        """
+        SNT:
+        The next three lines of code essentially drawa the ghost as a red dot in the center of the square.
+        For similar reasons the pac man describe above, the drawing location is 10 pixels up and horizontal.
+        """
+        up()
+        goto(point.x + 10, point.y + 10) #SNT: Centered the turtle before drawing the red dot, marking the ghost.
+        dot(20, 'red')
+        
+
+    update() #SNT: This function essentially forces the game to redraw everthing to do the most updated versions. 
+
+    for point, course in ghosts:
+        """
+        SNT:
+        This for loop essentially check if the pacman had collided with a ghost. The for loop essentially check for each and every ghost
+        In this game, each pac man and ghost is drawn as a circle of diameter of 20 pixles, or radius of 1o.
+        As such when the distance between them is less than 20, it means they have colided.
+        When this happens the function is interupted, and the game stops
+        """
+        """
+        HN: 
+        Whenever the difference between the two is below 20 (Which is only possible in the case of a collision with the ghost) the game abruptly
+        ends as nothing is called as the return here.
+        """
+        if abs(pacman - point) < 20: #SNT: abs(pacman-point) is the distance between ghost and pacman, which is used to determine collisions.
+            restart_level_1()
+
+    if levels == 3:
+        ontimer(move, 100)
+    if levels == 4:
+        count = 0
+        move = move_4
+        move()
+    """
+    SNT:
+    ontimer() is turtle function which takes in a function with no arguments for its first argument. In this case this is the function move(), which is the same function it is in.
+    The second arguments is the time intervals before the function in the first interval is re-called.
+    What this does is essentially after move() has been called, move() would carries out all its task and called itself again every 100 ticks of the computers, and keep repeating.
+    """
+    """
+    HN: 
+    The ontimer() function here is basically the determining the tickrate of the game, which is 0.1 seconds or 100 miliseconds in this context
+    as this function takes in time in terms of miliseconds. The tickrate determines how often the code is called.
+    """
+
+
+def move_4():
            
     """Move pacman and all ghosts."""
 
-    global aim, pacman, ghosts, tiles, count, move
+    global aim, pacman, ghosts, tiles, count, move, traveled
     
     if count == 0:
         """
         SNT:
         """
-        restart_level_1
-
-        def modified_games_rules_2(traveled):
-            """
-            SNT Modified Games Rules:
-            This function essentially keep track of all the tiles on which the pacman had traveled.
-            It then spawn a new ghost at that location, moving in the opposite direction as the pacman.
-            This function is then implemanted in the move() function.
-            """
-            just_traveled_tiles = offset(pacman)
-            if not(just_traveled_tiles in traveled): #SNT UpdatedL Only update the list if the pacman traveled to a new cell
-                traveled.append(just_traveled_tiles)
-
-            if len(traveled) > 7:
-
-                indice = traveled[0]
-                x = (indice % 20) * 20 - 200 
-                y = 180 - (indice // 20) * 20
-                ghosts.append([vector(x, y), -aim]) #SNT Updated: Add a new ghost in the last location the pac man travel.
+        restart_level_1()
 
         count += 1
  
+    def modified_games_rules_2():
+        """
+        SNT Modified Games Rules:
+        This function essentially keep track of all the tiles on which the pacman had traveled.
+        It then spawn a new ghost at that location, moving in the opposite direction as the pacman.
+        This function is then implemanted in the move() function.
+        """
+        just_traveled_tiles = offset(pacman)
+        if not(just_traveled_tiles in traveled): #SNT UpdatedL Only update the list if the pacman traveled to a new cell
+            traveled.append(just_traveled_tiles)
+
+        if len(traveled) > 7:
+
+            indice = traveled[0]
+            x = (indice % 20) * 20 - 200 
+            y = 180 - (indice // 20) * 20
+            ghosts.append([vector(x, y), -aim]) #SNT Updated: Add a new ghost in the last location the pac man travel.
+
 
     
     writer.undo() #SNT: Undo the what the previous turtle "writer" wrote.
@@ -646,7 +881,7 @@ def move_3():
         SNT Modified Game Rules:
         To make it fairer for the players, we make it so that only when the pacman had eaten a coins would a new ghost be spawned.
         """
-        modified_games_rules_2(traveled)
+        modified_games_rules_2()
 
         state['score'] += 1 #SNT: The user score is increase by 1.
         x = (index % 20) * 20 - 200 #SNT: The next three line redraw the tiles, this time without the "coins" in the middle
@@ -690,7 +925,13 @@ def move_3():
         if abs(pacman - point) < 20: #SNT: abs(pacman-point) is the distance between ghost and pacman, which is used to determine collisions.
             restart_level_1() #Since level 3 and 2 shares the same map, we used the same function to restart.
 
-    ontimer(move, 100)
+    if levels == 4:        
+        ontimer(move, 100)
+    if levels == 5:
+        count = 0 #SNT: Reinitialized the count
+        move = move_3 #SNT: Change move() to move_4() which is level 4
+        move() #SNT: Activates the function so ontimer() works
+
     
     
 
@@ -726,7 +967,6 @@ onkey(lambda: change(5, 0), 'Right')  #SNT: When the right arrow is pressed, the
 onkey(lambda: change(-5, 0), 'Left') #SNT: When the left arrow is pressed, the pacman turns left
 onkey(lambda: change(0, 5), 'Up') #SNT: When the up arrow is pressed, the pacman goes up
 onkey(lambda: change(0, -5), 'Down') #SNT: When the down arrow is pressed, the pacman goes down
-
 world() #SNT: Intialized the coordinates
 
 x_1 = (21 % 20) * 20 - 200 #SNT: The next three line redraw the tiles, this time without the "coins" in the middle
